d54a77171ad13df108aec48d3a656db69b8d06b8
flowerinsnow <flowerimsnow@hotmail.com>
2023-04-28 18:42:56 +0800

    为/haspermission命令的permission参数添加自动补全

--- common/src/main/java/team/floracore/common/commands/player/HasPermissionCommand.java
+++ common/src/main/java/team/floracore/common/commands/player/HasPermissionCommand.java
@@ -4,6 +4,9 @@ import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import cloud.commandframework.annotations.CommandPermission;
+import cloud.commandframework.annotations.suggestions.Suggestions;
+import cloud.commandframework.context.CommandContext;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +14,9 @@ import team.floracore.common.command.AbstractFloraCoreCommand;
 import team.floracore.common.locale.Message;
 import team.floracore.common.plugin.FloraCorePlugin;
 import team.floracore.common.sender.Sender;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CommandDescription("检查玩家是否拥有目标权限")
 @CommandPermission("floracore.command.haspermission")
@@ -24,10 +30,16 @@ public class HasPermissionCommand extends AbstractFloraCoreCommand {
     public void execute(
             @NotNull CommandSender s,
             @NotNull @Argument("target") Player target,
-            @NotNull @Argument("permission") String permission
+            @NotNull @Argument(value = "permission", suggestions = "permission_list") String permission
     ) {
         Sender sender = getPlugin().getSenderFactory().wrap(s);
         (target.hasPermission(permission) ? Message.COMMAND_HASPERMISSION_YES : Message.COMMAND_HASPERMISSION_NO)
                 .send(sender, target.getName(), permission);
+    }
+
+    @Suggestions("permission_list")
+    public @NotNull List<String> getPermissionList(@NotNull CommandContext<CommandSender> sender, @NotNull String input) {
+        return Bukkit.getPluginManager().getPermissions()
+                .stream().collect(ArrayList::new, (list, element) -> list.add(element.getName()), ArrayList::addAll);
     }
 }
