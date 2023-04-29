22615774ac3ca36f08112a8c77f85534af547656
xLikeWATCHDOG <bilibili.mer@outlook.com>
2023-04-29 22:00:34 +0800

    对服务器异常关闭时Chat的EndTime进行处理。

--- common/src/main/java/team/floracore/common/locale/chat/ChatManager.java
+++ common/src/main/java/team/floracore/common/locale/chat/ChatManager.java
@@ -3,6 +3,8 @@ package team.floracore.common.locale.chat;
 import team.floracore.common.plugin.*;
 import team.floracore.common.storage.misc.floracore.tables.*;
 
+import java.util.*;
+
 public class ChatManager {
     private final FloraCorePlugin plugin;
     private final Chat chat;
@@ -12,6 +14,16 @@ public class ChatManager {
         long startTime = System.currentTimeMillis();
         plugin.getStorage().getImplementation().insertChat(plugin.getServerName(), startTime);
         this.chat = plugin.getStorage().getImplementation().selectChatWithStartTime(plugin.getServerName(), startTime);
+        plugin.getBootstrap().getScheduler().async().execute(() -> {
+            List<Chat> ret = plugin.getStorage().getImplementation().selectChat(plugin.getServerName());
+            for (Chat i : ret) {
+                if (i.getId() != this.chat.getId()) {
+                    if (i.getEndTime() <= 0) {
+                        i.setEndTime(startTime);
+                    }
+                }
+            }
+        });
     }
 
     public Chat getChat() {
